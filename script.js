81535e90af89f29b880b74627c4c993a97f777ce
Anna Burley <anna.burley3@gmail.com>
2018-02-19 21:59:18 +0000

    Version7

---
+++ script.js
@@ -0,0 +1,67 @@
+// Code goes here
+
+var todoList = {
+    todos: [],
+    displayTodos: function() {
+        if (this.todos.length === 0) {
+            console.log('Your todo list is empty!');
+        } else {
+            console.log('My Todos:');
+            for (var i = 0; i < this.todos.length; i++){
+                if (this.todos[i].completed === true) {
+                    console.log('(x)', this.todos[i].todoText);
+                } else {
+                    console.log('( )', this.todos[i].todoText);
+                }
+            }
+        }
+    },
+    addTodo: function(todoText){
+        this.todos.push({
+            todoText: todoText,
+            completed: false
+        });
+        this.displayTodos();
+    },
+    changeTodo: function(position, todoText){
+        this.todos[position].todoText = todoText;
+        this.displayTodos();
+    },
+    deleteTodo: function(position){
+        this.todos.splice(position);
+        this.displayTodos();
+    },
+    toggleCompleted: function(position) {
+        var todo = this.todos[position]; //
+        todo.completed = !todo.completed;//
+        this.displayTodos();
+    },
+    //get number of completed todos
+    toggleAll: function () {
+        var totalTodos = this.todos.length;
+        var completedTodos = 0;
+        for (var i = 0; i < totalTodos; i++ ){
+            if(this.todos[i].completed === true) {
+                completedTodos++;
+            }
+        }
+        // Case 1
+        if(completedTodos === totalTodos) {
+            for (var i = 0; i < totalTodos; i++) {
+                this.todos[i].completed = false;
+                //Case 2
+            }
+        } else {
+            for (var i = 0; i < totalTodos; i++) {
+                this.todos[i].completed = true;
+            }
+        }
+
+        this.displayTodos();
+    }
+};
+var displayTodosButton = document.getElementById('displayTodosButton');
+console.log(displayTodosButton);
+displayTodosButton.addEventListener('click', function(){
+    todoList.displayTodos();
+});
